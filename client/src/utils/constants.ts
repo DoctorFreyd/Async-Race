826225acefee4b82cf2a03518a484e9ebb21c88c
Winston Smith <winstonnn.smith.1984@gmail.com>
2025-09-25 11:32:00 +0300

    feat: add constants util for car generation

---
+++ client/src/utils/constants.ts
@@ -0,0 +1,36 @@
+export const CAR_CONSTANTS = {
+  defaultGenerateCount: 100,
+  defaultChunkSize: 10,
+  colorHexBase: 16,
+  colorHexLength: 6,
+};
+
+export const MAKES = [
+  'Tesla',
+  'Mercedes',
+  'BMW',
+  'Audi',
+  'Chevrolet',
+  'Toyota',
+  'Honda',
+  'Nissan',
+  'Kia',
+  'Hyundai',
+  'Mazda',
+  'Subaru',
+];
+
+export const MODELS = [
+  'Model S',
+  'Benz',
+  '3 Series',
+  'A4',
+  'Camaro',
+  'Corolla',
+  'Civic',
+  'Altima',
+  'Sorento',
+  'Elantra',
+  'CX-5',
+  'Impreza',
+];

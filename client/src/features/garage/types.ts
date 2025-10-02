f2813e9851594747ace8bcd34ac00f819d443819
Winston Smith <winstonnn.smith.1984@gmail.com>
2025-10-02 03:26:53 +0300

    feat: add CarRaceState and CarDTO interfaces, extend Car type

--- client/src/features/garage/types.ts
+++ client/src/features/garage/types.ts
@@ -1,6 +1,16 @@
-export interface Car {
+export interface CarDTO {
   id: number;
   name: string;
   color: string;
+}
+
+export interface CarRaceState {
+  isMoving: boolean;
+  position: number;
+  durationMs: number;
+}
+
+export interface Car extends CarDTO {
   selected?: boolean;
+  race?: CarRaceState;
 }
